9ed10809beb18226b3806635dd8ac78cfa81e252
Luca Fornasari <fornaeffe@gmail.com>
2022-06-22 16:38:01 +0200

    art. 77 commi d, e, f

--- main.R
+++ main.R
@@ -210,5 +210,71 @@ cifre_uni$VOTI_DA_RESTI[
   is.na(cifre_uni$VOTI_DA_RESTI) | is.nan(cifre_uni$VOTI_DA_RESTI)
 ] <- 0
 
-cifre_uni$CIFRA <- cifre_uni$VOTI_LISTA + cifre_uni$PARTE_INTERA + cifre_uni$VOTI_DA_RESTI
+cifre_uni$CIFRA <- 
+  cifre_uni$VOTI_LISTA + 
+  cifre_uni$PARTE_INTERA + 
+  cifre_uni$VOTI_DA_RESTI
+
+# d) determina la cifra elettorale di collegio plurinominale di
+# ciascuna lista. Tale cifra e' data dalla somma delle cifre elettorali
+# di collegio uninominale di ciascuna lista;
+
+cifre_pluri <- aggregate(
+  CIFRA ~
+    CIRCOSCRIZIONE +
+    COLLEGIOPLURINOMINALE +
+    COALIZIONE +
+    LISTA,
+  data = cifre_uni,
+  sum,
+  na.action = na.pass
+)
+
+# e) determina la cifra elettorale percentuale di collegio
+# plurinominale di ciascuna lista. Tale cifra e' data dal quoziente
+# risultante dalla divisione della cifra elettorale di collegio
+# plurinominale di ciascuna lista per il totale dei voti validi del
+# rispettivo collegio plurinominale, moltiplicato per cento;
+
+totali_pluri <- aggregate(
+  CIFRA ~
+    CIRCOSCRIZIONE +
+    COLLEGIOPLURINOMINALE,
+  data = cifre_pluri,
+  sum,
+  na.action = na.pass
+)
+
+cifre_pluri <- merge(
+  cifre_pluri,
+  totali_pluri,
+  by = c(
+    "CIRCOSCRIZIONE",
+    "COLLEGIOPLURINOMINALE"
+  ),
+  suffixes = c("", "_TOT")
+)
+
+cifre_pluri$CIFRA_PERCENTUALE <- cifre_pluri$CIFRA / cifre_pluri$CIFRA_TOT * 100
+
+# f) determina la cifra elettorale circoscrizionale di ciascuna
+# lista. Tale cifra e' data dalla somma delle cifre elettorali di
+# collegio plurinominale della lista stessa;
+
+cifre_circ <- aggregate(
+  CIFRA ~
+    CIRCOSCRIZIONE +
+    COALIZIONE +
+    LISTA,
+  data = cifre_pluri,
+  sum,
+  na.action = na.pass
+)
+
+# g) determina la cifra elettorale percentuale di ciascun candidato
+# nel collegio uninominale. Tale cifra e' data dal quoziente risultante
+# dalla divisione della cifra elettorale individuale di ciascun
+# candidato per il totale dei voti validi del rispettivo collegio
+# uninominale, moltiplicato per cento;
+
 
