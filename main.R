b7aa997b648b7eed49ba44e286316079b7919585
Luca Fornasari <fornaeffe@gmail.com>
2022-06-26 14:20:03 +0200

    Art. 83 comma 1 lett. i

--- main.R
+++ main.R
@@ -39,63 +39,49 @@ source("C_83_1_g.R")
 ##### Art. 83 comma 1 lettera h - riparto circoscrizionale ####
 source("C_83_1_h.R")
 
-# i) procede quindi all'attribuzione nelle singole circoscrizioni
-# dei seggi spettanti alle liste di ciascuna coalizione. A tale fine,
-# determina il quoziente circoscrizionale di ciascuna coalizione di
-# liste dividendo il totale delle cifre elettorali circoscrizionali
-# delle liste ammesse alla ripartizione ai sensi della lettera g),
-# primo periodo, per il numero dei seggi assegnati alla coalizione
-# nella circoscrizione ai sensi della lettera h). Nell'effettuare la
-# divisione di cui al periodo precedente non tiene conto dell'eventuale
-# parte frazionaria del quoziente. Divide quindi la cifra elettorale
-# circoscrizionale di ciascuna lista della coalizione per tale
-# quoziente circoscrizionale. La parte intera del quoziente cosi'
-# ottenuto rappresenta il numero dei seggi da assegnare a ciascuna
-# lista. I seggi che rimangono ancora da attribuire sono assegnati alle
-# liste seguendo la graduatoria decrescente delle parti decimali dei
-# quozienti cosi' ottenuti; in caso di parita', sono attribuiti alle
-# liste con la maggiore cifra elettorale circoscrizionale; a parita' di
-# quest'ultima, si procede a sorteggio. Esclude dall'attribuzione di
-# cui al periodo precedente le liste alle quali e' stato attribuito il
-# numero di seggi ad esse assegnato a seguito delle operazioni di cui
-# alla lettera g). Successivamente l'ufficio accerta se il numero dei
-# seggi assegnati in tutte le circoscrizioni a ciascuna lista
-# corrisponda al numero dei seggi ad essa attribuito ai sensi della
-# lettera g). In caso negativo, procede alle seguenti operazioni,
-# iniziando dalla lista che abbia il maggior numero di seggi eccedenti
-# e, in caso di parita' di seggi eccedenti da parte di piu' liste, da
-# quella che abbia ottenuto la maggiore cifra elettorale nazionale,
-# proseguendo poi con le altre liste, in ordine decrescente di seggi
-# eccedenti: sottrae i seggi eccedenti alla lista nelle circoscrizioni
-# nelle quali essa li ha ottenuti con le parti decimali dei quozienti,
-# secondo il loro ordine crescente, e nelle quali inoltre le liste, che
-# non abbiano ottenuto il numero di seggi spettante, abbiano parti
-# decimali dei quozienti non utilizzate. Conseguentemente, assegna i
-# seggi a tali liste. Qualora nella medesima circoscrizione due o piu'
-# liste abbiano parti decimali dei quozienti non utilizzate, il seggio
-# e' attribuito alla lista con la piu' alta parte decimale del
-# quoziente non utilizzata o, in caso di parita', a quella con la
-# maggiore cifra elettorale nazionale. Nel caso in cui non sia
-# possibile attribuire il seggio eccedentario nella medesima
-# circoscrizione, in quanto non vi siano liste deficitarie con parti
-# decimali di quozienti non utilizzate, l'Ufficio prosegue, per la
-# stessa lista eccedentaria, nell'ordine dei decimali crescenti, a
-# individuare un'altra circoscrizione, fino a quando non sia possibile
-# sottrarre il seggio eccedentario e attribuirlo ad una lista
-# deficitaria nella medesima circoscrizione. Nel caso in cui non sia
-# possibile fare riferimento alla medesima circoscrizione ai fini del
-# completamento delle operazioni precedenti, fino a concorrenza dei
-# seggi ancora da cedere, alla lista eccedentaria vengono sottratti i
-# seggi nelle circoscrizioni nelle quali li ha ottenuti con le minori
-# parti decimali del quoziente di attribuzione e alle liste deficitarie
-# sono conseguentemente attribuiti seggi nelle altre circoscrizioni
-# nelle quali abbiano le maggiori parti decimali del quoziente di
-# attribuzione non utilizzate.
-# 2. L'Ufficio centrale nazionale provvede a comunicare ai singoli
-# Uffici centrali circoscrizionali il numero dei seggi assegnati a
-# ciascuna lista.
-# 3. Di tutte le operazioni dell'Ufficio centrale nazionale viene
-# redatto, in duplice esemplare, un apposito verbale: un esemplare e'
-# rimesso alla Segreteria generale della Camera dei deputati, la quale
-# ne rilascia ricevuta; un altro esemplare e' depositato presso la
-# cancelleria della Corte di cassazione.
+##### Art. 83 comma 1 lettera i - riparto circoscrizionale interno alle coalizioni ####
+source("C_83_1_i.R")
+
+
+# Art. 83-bis.
+# ((1. L'Ufficio centrale circoscrizionale, ricevute da parte
+# dell'Ufficio elettorale centrale nazionale le comunicazioni di cui
+#   all'articolo 83, comma 2, procede all'attribuzione nei singoli
+#   collegi plurinominali dei seggi spettanti alle liste. A tale fine
+#   l'ufficio determina il quoziente elettorale di collegio dividendo la
+# somma delle cifre elettorali di collegio di tutte le liste per il
+# numero dei seggi da attribuire nel collegio stesso. Nell'effettuare
+#   tale divisione non tiene conto dell'eventuale parte frazionaria del
+# quoziente. Divide quindi la cifra elettorale di collegio di ciascuna
+# lista per tale quoziente di collegio. La parte intera del quoziente
+# cosi' ottenuto rappresenta il numero dei seggi da assegnare a
+#   ciascuna lista. I seggi che rimangono ancora da attribuire sono
+#   assegnati alle liste seguendo la graduatoria decrescente delle parti
+#   decimali dei quozienti cosi' ottenuti; in caso di parita', sono
+#   attribuiti alle liste con la maggiore cifra elettorale
+#   circoscrizionale; a parita' di quest'ultima, si procede a sorteggio.
+#   L'Ufficio esclude dall'attribuzione di cui al periodo precedente le
+#   liste alle quali e' stato attribuito il numero di seggi ad esse
+# assegnato nella circoscrizione secondo la comunicazione di cui
+# all'articolo 83, comma 2. Successivamente l'ufficio accerta se il
+# numero dei seggi assegnati in tutti i collegi a ciascuna lista
+# corrisponda al numero di seggi ad essa attribuito nella
+# circoscrizione dall'Ufficio elettorale centrale nazionale. In caso
+#   negativo, determina la lista che ha il maggior numero di seggi
+#   eccedentari e, a parita' di essi, la lista che tra queste ha ottenuto
+# il seggio eccedentario con la minore parte decimale del quoziente;
+# sottrae quindi il seggio a tale lista nel collegio in cui e' stato
+#   ottenuto con la minore parte decimale dei quozienti di attribuzione e
+#   lo assegna alla lista deficitaria che ha il maggior numero di seggi
+#   deficitari e, a parita' di essi, alla lista che tra queste ha la
+# maggiore parte decimale del quoziente che non ha dato luogo
+# all'assegnazione di seggio; il seggio e' assegnato alla lista
+# deficitaria nel collegio plurinominale in cui essa ha la maggiore
+# parte decimale del quoziente di attribuzione non utilizzata; ripete
+# quindi, in successione, tali operazioni sino all'assegnazione di
+#   tutti i seggi eccedentari alle liste deficitarie)).
+# 2. Di tutte le operazioni dell'Ufficio centrale circoscrizionale
+# viene redatto, in duplice esemplare, apposito verbale: un esemplare
+# e' rimesso alla Segreteria generale della Camera dei deputati, la
+# quale ne rilascia ricevuta; un altro esemplare e' depositato presso
+# la cancelleria della Corte di cassazione.
