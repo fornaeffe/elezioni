c754ef7cf3bdbeb3aeb658df645d73b555b476c2
Luca Fornasari <fornaeffe@gmail.com>
2025-12-28 10:51:23 +0100

    spostato creazione scenario in R

--- R/scenario_file_creation.R
+++ R/scenario_file_creation.R
@@ -1,26 +1,23 @@
-library(data.table)
-library(writexl)
-
 crea_file_scenario <- function(
     dati,
     percorso = "scenario.xlsx",
     sovrascrivi = FALSE
 ) {
   # --- Controlli preliminari ----------------------------------------
-  stopifnot(is.data.frame(dati))
+  stopifnot(is.data.frame(dati$comuni_liste_elezioni))
   
   if (file.exists(percorso) && !sovrascrivi) {
     stop(paste("Il file", percorso, "esiste già. Usa sovrascrivi = TRUE per ricrearlo."))
   }
   
   # --- 1. liste_future (vuoto) -------------------------------------
-  liste_future <- data.table(LISTA_FUTURA = character(), COALIZIONE = character(), COLORE = character())
+  liste_future <- data.table::data.table(LISTA_FUTURA = character(), COALIZIONE = character(), COLORE = character())
   
   # --- 2. flussi_previsti ------------------------------------------
   colonne_flussi <- c(
     "DATA", "ELEZIONE", "LISTA"
   )
-  flussi_previsti <- unique(dati[, ..colonne_flussi])
+  flussi_previsti <- unique(dati$comuni_liste_elezioni[, ..colonne_flussi])
   flussi_previsti[, `:=`(LISTA_FUTURA = "", FRAZIONE = 1)]
   
   # --- Scrittura file Excel ----------------------------------------
